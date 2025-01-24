44bc26e1a25dfadcfbee94eb4960f2fc0dc857e4
zc_1113 <zc_1113@outlook.com>
2025-01-24 16:54:09 +0800

    2025.1.24

--- code/ASID.v
+++ code/ASID.v
@@ -8,12 +8,9 @@ module ASID (
     output  reg [31:0]  ASID
 );
     always @(posedge clk or negedge rst_n) begin
-        if(!rst_n) begin
+        if(!rst_n)
             ASID <= {8'b0, 8'b10, 6'b0, 10'b0};
-        end else begin
-            if(ASID_wr_en) begin
-                ASID <= ASID_wr;
-            end
-        end
+        else if(ASID_wr_en)
+            ASID <= ASID_wr;
     end
 endmodule
