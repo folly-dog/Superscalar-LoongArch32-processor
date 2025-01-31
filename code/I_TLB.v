2a639790c1ffaca62ebde3deb74c8c7b3f3cf72c
zc_1113 <zc_1113@outlook.com>
2025-01-31 22:24:59 +0800

    2025.1.31

--- code/I_TLB.v
+++ code/I_TLB.v
@@ -2,38 +2,24 @@ module I_TLB (
     input               clk,
     input               rst_n,
 
-    input               VPN_en,
-    input  [31:12]      VPN,
-    input  [23:12]      page_mask,
-
-    input               I_TLB_VPN_invld,
-    input   [31:0]      I_TLB_VPN_invld_reg,
-
-    input               I_TLB_ASID_invld,
-    input   [31:0]      I_TLB_ASID_invld_reg,
-    
-    input               I_TLB_locked_invld,
-
-    input               I_TLB_rd_en,
-    input               I_TLB_wr_en,
-    input    [4:0]      I_TLB_wr_addr,
-    input   [51:0]      I_TLB_wr_data,
-    output  [51:0]      I_TLB_rd_data,
-
-    output         reg  I_TLB_hit,
-    output [31:12] reg  PPN,
-
-    output         reg  I_TLB_2_MMU_en,
-    output [31:12] reg  I_TLB_VPN_2_MMU,
-    input               I_TLB_PPN_from_MMU_en,
-    input  [31:12]      I_TLB_PPN_from_MMU,
-    input               I_TLB_from_MMU_page_fault,
-    output              I_TLB_page_fault
 );
 
-    reg  [31:12] VPN_reg  [31:0];
-    reg  [31:12] PPN_reg  [31:0];
-    reg    [7:0] ASID_reg [31:0];
-    reg
+    reg  [31:12] ITLB_VPN  [31:0];
+    reg    [5:0] ITLB_PS   [31:0];
+    reg          ITLB_G    [31:0];
+    reg    [9:0] ITLB_ASID [31:0];
+    reg          ITLB_EN   [31:0];
+
+    reg  [31:12] ITLB_PPN_0  [31:0];    //0表示奇数页
+    reg    [1:0] ITLB_PLV_0  [31:0];
+    reg    [1:0] ITLB_MAT_0  [31:0];
+    reg    [1:0] ITLB_PLV_0  [31:0];
+    reg    [1:0] ITLB_MESI_0 [31:0];
+
+    reg  [31:12] ITLB_PPN_1  [31:0];    //1表示奇数页
+    reg    [1:0] ITLB_PLV_1  [31:0];
+    reg    [1:0] ITLB_MAT_1  [31:0];
+    reg    [1:0] ITLB_PLV_1  [31:0];
+    reg    [1:0] ITLB_MESI_1 [31:0];
 
 endmodule
