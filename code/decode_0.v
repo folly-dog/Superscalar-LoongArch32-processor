6e26cb845cbbe704d08e1aa5f976f97a91f49d9b
zc_1113 <zc_1113@outlook.com>
2025-02-21 23:15:58 +0800

    2025.2.21 verison1

--- code/decode_0.v
+++ code/decode_0.v
@@ -12,16 +12,17 @@ module decode_0 (
     input       [31:0]  instruction,
 
     output  reg [31:0]  PC_stage3,
-    output  reg  [2:0]  except_stage3,
+    output  reg [14:0]  except_code_stage3,
 
     output  reg         decode_vld,
     output  reg  [3:0]  IQ_choose,      // 0000无需写入IQ
-    output  reg             PLV,
 
     output  reg [4:0]   ALU0_op,
     output  reg [2:0]   ALU1_op,        // 乘除取余
     output  reg [3:0]   AGU_op,
     output  reg [3:0]   BRU_op,
+
+    output  reg [3:0]   ROB_op,         // retire时，ROB需要进行一些操作，包括store指令和一些特权指令
 
     output  reg [19:0]  ALU0_imm,       // 正常为12 bit, 将load立即数功能加入其中
     output  reg [13:0]  AGU_imm,        
@@ -40,11 +41,12 @@ module decode_0 (
 );  
     wire [31:0] PC_stage3_wire;
 
-    reg  [2:0]  except_stage3_wire;
+    reg  [14:0]  except_code_stage3_wire;
 
     wire        decode_vld_wire;
     reg  [3:0]  IQ_choose_wire;      // 0000无需写入IQ
-    reg             PLV_wire;
+    reg         PLV_wire;
+    reg  [3:0]  ROB_op_wire;
 
     reg  [4:0]   ALU0_op_wire;
     reg  [2:0]   ALU1_op_wire;        // 乘除取余
@@ -64,13 +66,32 @@ module decode_0 (
     reg          source2_reg_en_wire;
     reg  [4:0]   source2_reg_wire;
 
+    wire ERTN;
+    wire IDLE;
+    wire BREAK;
+    wire SYSCALL;
+    wire DBAR;
+    wire IBAR;
+
+    assign ERTN = (instruction == 32'b00000110010010000011010000000000);
+    assign IDLE = (instruction[31:15] == 17'b000001100100101);
+    assign BREAK = (instruction[31:15] == 17'b00000000001010100);
+    assign SYSCALL = (instruction[31:15] == 17'b00000000001010110);
+    assign DBAR = (instruction[31:15] == 17'b00111000011100100);
+    assign IBAR = (instruction[31:15] == 17'b00111000011100101);
+
     assign PC_stage3_wire = {PC_stage2[31:4], 2'b00, 2'b00};
 
-    assign decode_vld_wire = (instruction_vld && (PLV_wire || (|IQ_choose_wire)));  // decode_vld_wire
+    assign decode_vld_wire = (instruction_vld && 
+                (BREAK || SYSCALL || PLV_wire || DBAR || IBAR || (|IQ_choose_wire)));  // decode_vld_wire
 
-    always @(*) begin           // except_stage3_wire
-        except_stage3_wire[1:0] = except_stage2;
-        except_stage3_wire[2] = (instruction_vld && (!decode_vld_wire));
+    always @(*) begin           // except_code_stage3_wire
+        if(|except_stage2)
+            except_code_stage3_wire = {13'd0,except_stage2};
+        else if(BREAK | SYSCALL)
+            except_code_stage3_wire = (instruction[14:0]);
+        else
+            except_code_stage3_wire = 15'd0;
     end
 
     always @(*) begin           // IQ_choose_wire
@@ -123,8 +144,26 @@ module decode_0 (
                                                 4'b1100: IQ_choose_wire = 4'b1000;
                                                 4'b1001: IQ_choose_wire = 4'b1000;
                                                 4'b1000: IQ_choose_wire = 4'b1000;
+                                                4'b0000: begin
+                                                    if({instruction[16:10], instruction[4:0]} == 12'b001100000000)
+                                                        IQ_choose_wire = 4'b1000;
+                                                    else if(instruction[16:5] == 12'b001100000000)
+                                                        IQ_choose_wire = 4'b1000;
+                                                    else if(instruction[16:5] == 12'b001100100000)
+                                                        IQ_choose_wire = 4'b1000;
+                                                    else
+                                                        IQ_choose_wire = 4'b0000;
+                                                end
                                                 default: IQ_choose_wire = 4'b0000;
                                             endcase
+                                    end
+                                    else if(instruction[22] && ({instruction[21:20],instruction[17:15]} == 5'b00001))begin
+                                        case (instruction[19:18])
+                                            2'b00: IQ_choose_wire = 4'b1000;
+                                            2'b01: IQ_choose_wire = 4'b1000;
+                                            2'b10: IQ_choose_wire = 4'b1000;
+                                            default: IQ_choose_wire = 4'b0000;
+                                        endcase
                                     end
                                     else
                                         IQ_choose_wire = 4'b0000;
@@ -138,6 +177,10 @@ module decode_0 (
     always @(*) begin           // PLV_wire
         if(!instruction_vld)
             PLV_wire = 1'b0;
+        else if (ERTN)
+            PLV_wire = 1'b1;
+        else if (IDLE)
+            PLV_wire = 1'b1;
         else begin
             if(instruction[31:26] == 6'b000001)begin
                 if(instruction[25:24] == 2'b00)
@@ -184,7 +227,14 @@ module decode_0 (
                     3'b111: ALU0_op_wire = 5'b00110;     // XORI
                     default: ALU0_op_wire = 5'b00000;
                 endcase
-            else
+            else if(instruction[22])
+                case (instruction[19:18])
+                    2'b00: ALU0_op_wire = 5'b10101;     // SLLI.W
+                    2'b01: ALU0_op_wire = 5'b10110;     // SRLI.W
+                    2'b10: ALU0_op_wire = 5'b10111;     // SRAI.W
+                    default: ALU0_op_wire = 5'b00000;
+                endcase
+            else if(instruction[20])
                 case (instruction[19:15])
                     5'b00000: ALU0_op_wire = 5'b00111;     // ADD.W
                     5'b00010: ALU0_op_wire = 5'b01000;     // SUB.W
@@ -199,6 +249,14 @@ module decode_0 (
                     5'b10000: ALU0_op_wire = 5'b10001;     // SRA.W
                     default: ALU0_op_wire = 5'b00000;
                 endcase
+            else begin
+                if(instruction[10])
+                    ALU0_op_wire = 5'b10010;     // RDCNTVH.W
+                else if(instruction[4:0] == 5'b00000)
+                    ALU0_op_wire = 5'b10100;     // RDCNTID
+                else
+                    ALU0_op_wire = 5'b10011;     // RDCNTVL.W
+            end
         end
         else
             ALU0_op_wire = 5'b00000;
@@ -303,5 +361,49 @@ module decode_0 (
     end
 
     assign instruction_target_stage3_wire = PC_stage2;
+
+    always @(*) begin           // ROB_op
+        if(BREAK)
+            ROB_op = 4'd1;  // break
+        else if(SYSCALL)
+            ROB_op = 4'd2;  // syscall
+        else 
+            case (instruction[29:24])
+                6'b101001: ROB_op = (instruction[23:22] == 2'b11) ? 4'd0 : 4'd3;    // store类
+                6'b000100: 
+                    case (instruction[9:5])
+                        5'd0:   ROB_op = 4'd4;  // CSRRD
+                        5'd1:   ROB_op = 4'd5;  // CSRWR
+                        default: ROB_op = 4'd6; // CSRXCHG
+                    endcase
+                6'b000110:
+                    case (instruction[23:22])
+                        2'b00: ROB_op = 4'd7;   // CACOP
+                        2'b01:begin
+                            if(instruction[21:13], instruction[9:0] == 19'b0010000010000000000)
+                                case (instruction[12:10])
+                                    3'b010: ROB_op = 4'd8;   // TLBSRCH
+                                    3'b011: ROB_op = 4'd9;   // TLBRD
+                                    3'b100: ROB_op = 4'd10;   // TLBWR
+                                    3'b101: ROB_op = 4'd11;   // TLBFILL
+                                    3'b110: ROB_op = 4'd12;   // ERTN
+                                    default: ROB_op = 4'd0;
+                                endcase
+                            else if(instruction[15])begin
+                                if(!instruction[16])
+                                    ROB_op = 4'd13;   // IDLE
+                                else
+                                    ROB_op = 4'd14;   // INVTLB
+                            end
+                            else
+                                ROB_op = 4'd0;
+                        end
+                        default: ROB_op = 4'd0;
+                    endcase
+                default: ROB_op = 4'd0;
+            endcase
+    end
+
+
 
 endmodule
